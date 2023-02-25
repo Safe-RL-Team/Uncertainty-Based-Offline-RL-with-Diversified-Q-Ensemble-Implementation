f45b792988d178e7461dda5b635fc9503935ae22
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2023-02-25 21:37:05 +0100

    video callback and more logging

--- edac.py
+++ edac.py
@@ -1,6 +1,7 @@
 import os
 from copy import deepcopy
 from dataclasses import dataclass
+from typing import Callable
 import pyrallis
 import torch
 import torch.nn as nn
@@ -26,7 +27,7 @@ class TrainConfig:
     env: str = 'halfcheetah-medium-v2'  # environment name
 
     num_critics: int = 5  # number of critics
-    beta = 0.1  # factor for action log probability for the actor loss
+    beta : float = 0.1  # factor for action log probability for the actor loss
     eta: float = 0.1  # eta for diversity loss
     gamma: float = 0.99  # discount factor
     tau: float = 0.005  # tau for target network update
@@ -82,6 +83,15 @@ class Actor(nn.Module):
         self.output = nn.Linear(layer_sizes[-2], layer_sizes[-1])
         self.output_uncertainty = nn.Linear(layer_sizes[-2], layer_sizes[-1])
 
+        # init as in the EDAC paper
+        for layer in self.hidden[::2]:
+            torch.nn.init.constant_(layer.bias, 0.1)
+        torch.nn.init.uniform_(self.output.weight, -1e-3, 1e-3)
+        torch.nn.init.uniform_(self.output.bias, -1e-3, 1e-3)
+        torch.nn.init.uniform_(self.output_uncertainty.weight, -1e-3, 1e-3)
+        torch.nn.init.uniform_(self.output_uncertainty.bias, -1e-3, 1e-3)
+
+
     def forward(self, state : torch.Tensor):
         x_hidden = self.hidden(state)
         x_mean = self.output(x_hidden)
@@ -105,14 +115,19 @@ class VectorCritic(nn.Module):
                 ]
             )) for _ in range(num_critics)
         ])
+        for model in self.models:
+            # init as in the EDAC paper
+            for layer in model[::2]:
+                torch.nn.init.constant_(layer.bias, 0.1)
+            torch.nn.init.uniform_(model[-1].weight, -3e-3, 3e-3)
+            torch.nn.init.uniform_(model[-1].bias, -3e-3, 3e-3)
     
     def forward(self, state: torch.Tensor, action: torch.Tensor):
         return torch.cat([model(torch.cat([state, action], dim=-1)) for model in self.models], dim=-1)
-        # return [model(torch.cat([state, action], dim=-1)) for model in self.models]
 
 
-@pyrallis.wrap()
-def train(config: TrainConfig) -> None:
+
+def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]], None]) -> None:
     # init env
     eval_env = gym.make(config.env)
     state_dim = eval_env.observation_space.shape[0]
@@ -169,7 +184,7 @@ def train(config: TrainConfig) -> None:
             q_gradients_tmp = torch.autograd.grad(critic(state_tmp, action_tmp).sum(), action_tmp, create_graph=True)[0].transpose(0,1)  # TODO: is action correct?
             q_gradients = q_gradients_tmp / (q_gradients_tmp.norm(p=2,dim=-1).unsqueeze(-1) + 1e-10)
             # [batch_size, num_critics, num_critics]
-            q_cosine_similarity_matrix = (q_gradients @ q_gradients.transpose(1,2)) * (1 - torch.eye(config.num_critics, device=config.device))  # TODO: it seems like nans arise in @
+            q_cosine_similarity_matrix = (q_gradients @ q_gradients.transpose(1,2)) * (1 - torch.eye(config.num_critics, device=config.device))
             # [1]
             diversity_loss = (q_cosine_similarity_matrix.sum(dim=(1,2))).mean() / (config.num_critics - 1)
             critic_loss = critic_losses + config.eta * diversity_loss
@@ -193,6 +208,15 @@ def train(config: TrainConfig) -> None:
         # eval
         actor.eval()
         with torch.no_grad():
+            if display_video_callback:
+                video = []
+                state = eval_env.reset()
+                done = False
+                while not done:
+                    action, _ = actor(torch.tensor(state, dtype=torch.float32, device=config.device))
+                    state, reward, done, _ = eval_env.step(action.cpu().numpy())
+                    video.append(eval_env.render(mode='rgb_array'))
+                display_video_callback(video)
             rewards = np.zeros(config.eval_episodes)
             for i in trange(config.eval_episodes, desc='Eval Episode', leave=False):
                 state = eval_env.reset()
@@ -201,21 +225,29 @@ def train(config: TrainConfig) -> None:
                     action, _ = actor(torch.tensor(state, dtype=torch.float32, device=config.device))
                     state, reward, done, _ = eval_env.step(action.cpu().numpy())
                     rewards[i] += reward
-                    # eval_env.render()
         actor.train()
 
         # log
         wandb.log({
             "epoch": epoch,
-            "critic_loss": critic_loss.item(),
-            "actor_loss": actor_loss.item(),
-            "eval/mean_reward": np.mean(rewards),
-            "eval/std_reward": np.std(rewards),
+            "critic/loss": critic_loss.item(),
+            "critic/base_loss": critic_losses.item(),
+            "critic/diversity_loss": diversity_loss.item(),
+            "actor/loss": actor_loss.item(),
             "actor/entropy": -actor_action_log_prob.mean().item(),
-            "actor/q_value": actor_q_values.mean().item(),
+            "actor/q_value_mean": actor_q_values.mean().item(),
+            "actor/q_value_std": actor_q_values.std().item(),
+            "eval/reward_mean": np.mean(rewards),
+            "eval/reward_std": np.std(rewards),
         })
 
     wandb_run.finish()
+
+
+
+@pyrallis.wrap()
+def main(config: TrainConfig) -> None:
+    train(config)
 
 
 

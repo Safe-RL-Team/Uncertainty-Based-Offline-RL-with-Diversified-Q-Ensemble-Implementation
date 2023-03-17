6ccbf2cd4069cabd2ba42c0b9ffd9836224ab687
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2023-03-17 13:54:13 +0100

    fix saving; learn beta for faster training

--- edac.py
+++ edac.py
@@ -31,6 +31,7 @@ class TrainConfig:
     batch_size: int = 2048  # batch size (per update)
     lr_actor: float = 0.0003  # learning rate for actor
     lr_critic: float = 0.0003  # learning rate for critic
+    lr_beta: float = 0.0003  # learning rate for beta
     env: str = 'halfcheetah-medium-v2'  # environment name
 
     num_critics: int = 5  # number of critics
@@ -45,7 +46,7 @@ class TrainConfig:
     seed: int = 0  # seed (0 for random seed)
     device: str = 'auto'  # device to use (auto, cuda or cpu)
 
-    save_path: str = 'ckp'  # where to save the model weights and config
+    save_path: str = 'ckp'  # where to save the model weights and config, None for no saving
     save_every: int = 10  # save the model every x epochs
     continue_from: str = ''  # continue training from a checkpoint file (config has to be loaded separately)
 
@@ -53,9 +54,9 @@ class TrainConfig:
         if self.device == 'auto':
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.name_full = f'{self.name}-{self.env}-{time.strftime("%y%m%d-%H%M%S")}'
-        if self.save_path.lower() != 'none':
+        if not self.save_path or self.save_path.lower() == 'none':
             self.save_path = None
-        if self.save_path:
+        else:
             self.save_path_full = Path(self.save_path) / self.name_full
 
 
@@ -63,7 +64,7 @@ class TrainConfig:
 class ReplayBuffer:
     '''buffer for an offline RL dataset'''
     def __init__(self, dataset, batch_size : int, device : str):
-        size = dataset["observations"].shape[0]
+        size = dataset['observations'].shape[0]
         if size < batch_size:
             raise ValueError(f'The batch_size ({batch_size}) cannot be larger than the dataset size ({size}).')
         self.batch_size = batch_size
@@ -183,9 +184,12 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
     critic = VectorCritic([state_dim + action_dim, 256, 256, 1], num_critics=config.num_critics).to(config.device)
     critic_optimizer = torch.optim.Adam(critic.parameters(), lr=config.lr_critic)
     with torch.no_grad(): target_critic = deepcopy(critic)  # make training more stable by using a soft updated target critic
-    # TODO: maybe also train config.beta, as in the CORL implementation (there its called alpha),
-    #       but the paper pseudocode doesn't do that.
-    #       the official implementaiton does that only if `use_automatic_entropy_tuning` is enabled
+    if config.lr_beta > 0:
+        # dynamically adjust beta (called alpha in CORL and the official implementation, which enables it if `use_automatic_entropy_tuning` is set)
+        # the paper pseudo-code does not mention this
+        log_beta = torch.tensor([np.log(config.beta)], dtype=torch.float32, requires_grad=True, device=config.device)
+        beta_optimizer = torch.optim.Adam([log_beta], lr=config.lr_actor)
+        beta = log_beta.exp().detach()
 
     # continue from checkpoint
     start_epoch = 0
@@ -199,6 +203,10 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
         target_critic.load_state_dict(checkpoint['target_critic'])
         actor_optimizer.load_state_dict(checkpoint['actor_optimizer'])
         critic_optimizer.load_state_dict(checkpoint['critic_optimizer'])
+        if config.lr_beta > 0:
+            log_beta.data[0] = checkpoint['log_beta']
+            beta = log_beta.exp().detach()
+            beta_optimizer.load_state_dict(checkpoint['beta_optimizer'])
         start_epoch = checkpoint['epoch'] + 1
         if start_epoch >= config.epochs:
             print(f'Already finished training: {start_epoch}/{config.epochs} epochs')
@@ -208,7 +216,7 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
     # save config
     if config.save_path:
         config.save_path_full.mkdir(parents=True, exist_ok=True)
-        with (config.save_path_full / 'config.yaml').open("w") as f:
+        with (config.save_path_full / 'config.yaml').open('w') as f:
             pyrallis.dump(config, f)
 
     # init wandb logging
@@ -218,13 +226,15 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
     for epoch in trange(start_epoch, config.epochs, desc='Epoch'):
         epoch_start_time = time.time()
         for _ in trange(config.updates_per_epoch, desc='Training Update', leave=False):
+            # TODO: maybe reoder how critics, actor and beta are updated
+
             # [batch_size, ...]
             state, action, reward, next_state, done = buffer.sample()
             with torch.no_grad():
                 # [batch_size, action_dim], [batch_size]
                 next_action, next_action_log_prob = actor(next_state)
                 # [batch_size]
-                q_next = target_critic(next_state, next_action).min(-1).values - config.beta * next_action_log_prob
+                q_next = target_critic(next_state, next_action).min(-1).values - beta * next_action_log_prob
                 # [batch_size]
                 q_target = reward + (1 - done) * config.gamma * q_next
 
@@ -251,10 +261,18 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
             critic_loss.backward()
             critic_optimizer.step()
 
-            # update actor
+            # update beta
             actor_action, actor_action_log_prob = actor(state)
+            if config.lr_beta > 0:
+                beta_loss = (-log_beta * (actor_action_log_prob - action_dim).detach()).mean()
+                beta_optimizer.zero_grad()
+                beta_loss.backward()
+                beta_optimizer.step()
+                beta = log_beta.exp().detach()
+
+            # update actor
             actor_q_values = critic(state, actor_action)
-            actor_loss = -(actor_q_values.min(-1).values - config.beta * actor_action_log_prob).mean()
+            actor_loss = -(actor_q_values.min(-1).values - beta * actor_action_log_prob).mean()
             actor_optimizer.zero_grad()
             actor_loss.backward()
             actor_optimizer.step()
@@ -295,8 +313,10 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
             torch.save(dict(
                 actor = actor.state_dict(),
                 critic = critic.state_dict(),
+                log_beta = log_beta.item() if config.lr_beta > 0 else None,
                 actor_optimizer = actor_optimizer.state_dict(),
                 critic_optimizer = critic_optimizer.state_dict(),
+                beta_optimizer = beta_optimizer.state_dict() if config.lr_beta > 0 else None,
                 target_critic = target_critic.state_dict(),
                 epoch = epoch,
             ), config.save_path_full / f'edac-{epoch}.pt')
@@ -304,20 +324,22 @@ def train(config: TrainConfig, display_video_callback: Callable[[list[np.array]]
 
         # log
         wandb.log({
-            "epoch": epoch,
-            "critic/loss": critic_loss.item(),
-            "critic/base_loss": base_critic_loss.item(),
-            "critic/diversity_loss": config.eta and diversity_loss.item(),
-            "critic/weight_std": torch.stack([torch.cat([p.flatten() for p in c.parameters()]) for c in critic.models]).std(dim=0).mean().item(),
-            "actor/loss": actor_loss.item(),
-            "actor/entropy": -actor_action_log_prob.mean().item(),
-            "actor/q_value_mean": actor_q_values.mean().item(),
-            "actor/q_value_std": actor_q_values.std().item(),
-            "eval/reward_mean": np.mean(rewards),
-            "eval/reward_std": np.std(rewards),
-            "time/epoch": time.time() - epoch_start_time,
-            "time/train": train_time,
-            "time/eval": eval_time,
+            'epoch': epoch,
+            'critic/loss': critic_loss.item(),
+            'critic/base_loss': base_critic_loss.item(),
+            'critic/diversity_loss': config.eta and diversity_loss.item(),
+            'critic/weight_std': torch.stack([torch.cat([p.flatten() for p in c.parameters()]) for c in critic.models]).std(dim=0).mean().item(),
+            'actor/loss': actor_loss.item(),
+            'actor/entropy': -actor_action_log_prob.mean().item(),
+            'actor/q_value_mean': actor_q_values.mean().item(),
+            'actor/q_value_std': actor_q_values.std().item(),
+            'beta/value': beta,
+            'beta/loss': config.lr_beta and beta_loss.item(),
+            'eval/reward_mean': np.mean(rewards),
+            'eval/reward_std': np.std(rewards),
+            'time/epoch': time.time() - epoch_start_time,
+            'time/train': train_time,
+            'time/eval': eval_time,
         })
 
     if config.save_path:
